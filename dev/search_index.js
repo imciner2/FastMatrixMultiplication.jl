cc750b409dd8f70ee7ad81e6b64f28c645dc1d01
Documenter.jl <documenter@juliadocs.github.io>
2023-07-07 10:49:54 +0000

    build based on 11a0a98

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FastMatrixMultiplication","category":"page"},{"location":"#FastMatrixMultiplication","page":"Home","title":"FastMatrixMultiplication","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FastMatrixMultiplication.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FastMatrixMultiplication]","category":"page"}]
+}
